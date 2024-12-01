8180245c37b08aefd036575610599dc927539b54
KurlanB <kurlan.bee@gmail.com>
2024-11-30 23:18:07 -0500

    Game loop - test6

--- src/frontend/the-matrix.c
+++ src/frontend/the-matrix.c
@@ -121,7 +121,7 @@ int main(int argc, char *argv[])
 
             display2048GUI(screenHeight, screenWidth, gameHeight, gameWidth, tilePadding, border, tileWidth, tileHeight, fontAdjustX, fontAdjustY, fontSize, grid);
 	
-	    	sleep(1);
+	    	sleep(0.5);
 		if(!fullTiles){
 			continue;
 		}
